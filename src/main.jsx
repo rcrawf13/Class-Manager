021e074113eb42ae4ba3c6d238841023d3d15201
rcrawf13 <richmondcrawford44@gmail.com>
2025-08-15 18:50:07 -0400

    Fixed jsx errors

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.js'
+import App from './App.jsx';
 import { LocalizationProvider } from '@mui/x-date-pickers'
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
 
